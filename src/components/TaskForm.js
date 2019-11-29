6d1b9d058fdad634ffab302f2d758020348901de
Cristian Guamán <cristian.guaman@icloud.com>
2019-11-29 03:50:42 -0500

    Building a Form

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -3,17 +3,35 @@ import React, { Component } from 'react';
 
 
 class TaskForm extends Component {
+    state = {
+        title: '',
+        description: '',   
+    }
+
     miSubmit = (e) => {
         console.log('Submiting');
         e.preventDefault();
+    }
+    miChange = (e) => {
+        console.info(e.target.name +" "+ e.target.value);
+        this.setState({
+           [ e.target.name]: e.target.value
+        })
     }
 
     render() {
         return (
             <form onSubmit={this.miSubmit} style={{'border':'1px solid #000', 'padding':'2em'}} >
-                <input type="text" placeholder="Write a Task"/>
+                <input onChange={this.miChange} 
+                type="text" 
+                placeholder="Write a Task" 
+                value={this.state.title}
+                name="title"/>
                 <br/><br/>
-                <textarea placeholder="Write a description"></textarea>
+                <textarea onChange={this.miChange} 
+                placeholder="Write a description" 
+                value={this.state.description}
+                name="description"></textarea>
                 <br/><br/>
                 <input type="submit" value="Save"/>
             </form>
